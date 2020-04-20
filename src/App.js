73858bcb44a66017c32cdf2a5193b880c94c6d58
Henrique Olivo Antonio <henriqueolivoantonio@outlook.com>
2020-04-20 18:36:53 -0300

    Setup redux configuration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Page from './page';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
-function App() {
+import './config/ReactotronConfig';
+
+import { store, persistor } from './store';
+
+import GlobalStyle from './globalStyle';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <GlobalStyle />
+        <Page />
+      </PersistGate>
+    </Provider>
   );
 }
-
-export default App;
